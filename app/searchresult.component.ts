02ee5b1211f54f46dcef3c642d5828a752843cf4
Mads Vøge <mqv@avaleo.net>
2016-06-16 14:50:37 +0200

    Readded the accoridan around the cards

--- app/searchresult.component.ts
+++ app/searchresult.component.ts
@@ -5,11 +5,12 @@ import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
+import {Header} from 'primeng/primeng';
 
 @Component({
     selector: 'search-result',
     templateUrl: 'app/views/searchresult.html',
-    directives: [MovieDetailsComponent, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_LIST_DIRECTIVES, MD_ICON_DIRECTIVES]
+    directives: [MovieDetailsComponent, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_LIST_DIRECTIVES, MD_ICON_DIRECTIVES, Header]
 })
 export class SearchResultComponent {
     @Input()
--- app/searchresults.component.ts
+++ app/searchresults.component.ts
@@ -5,11 +5,15 @@ import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
+import { Accordion } from 'primeng/primeng';
+import { AccordionTab } from 'primeng/primeng';
+import {Header} from 'primeng/primeng';
+import { MovieDetailsComponent } from './movie-details.component';
 
 @Component({
     selector: 'search-results',
     templateUrl: 'app/views/searchresults.html',
-    directives: [SearchResultComponent, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_LIST_DIRECTIVES, MD_ICON_DIRECTIVES],
+    directives: [SearchResultComponent, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_LIST_DIRECTIVES, MD_ICON_DIRECTIVES, Accordion, AccordionTab, Header, MovieDetailsComponent],
     providers: [MdIconRegistry]
 })
 export class SearchResultsComponent {
