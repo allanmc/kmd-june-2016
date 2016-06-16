db464e17f0eb7aac188fa748fd90c7b66e7e8075
Allan Mørk Christensen <allanmc@gmail.com>
2016-06-16 14:18:12 +0200

    Chaged to Angular Material

--- systemjs.config.js
+++ systemjs.config.js
@@ -9,12 +9,14 @@
         '@angular':                   'node_modules/@angular',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         'rxjs':                       'node_modules/rxjs',
+        'primeng':                    'node_modules/primeng',
         '@angular2-material':         'node_modules/@angular2-material'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app':                        { main: 'main.js',  defaultExtension: 'js' },
         'rxjs':                       { defaultExtension: 'js' },
+        'primeng':                    { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' }
     };
 
