fee25481c7d9934e6a7034f15f3d320fe41e6e14
David Brewster <qdb@avaleo.net>
2016-06-16 13:13:47 +0200

    Replacing stub with service call to ombdapi

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OmdbService } from './omdb.service';
 import { Result } from './result';
+import { HTTP_PROVIDERS } from '@angular/http';
+
 import { SearchResultsComponent } from './searchresults.component';
 import { OmdbSearchComponent } from './omdbsearch.component';
 
@@ -11,15 +13,18 @@ import { OmdbSearchComponent } from './omdbsearch.component';
     <omdb-search></omdb-search>
     <h2>My Results</h2>
     <search-results [results]="results"></search-results>
+    <p>Title: <input type="text" name="title" style="width: 100px" [(ngModel)]="searchField" (keyup.enter)="getResults()"/></p>
   `,
     directives: [SearchResultsComponent, OmdbSearchComponent],
-    providers: [OmdbService]
+    providers: [OmdbService, HTTP_PROVIDERS]
 })
 export class AppComponent implements OnInit {
-    results: Result[];
+    searchField: String = "1984";
+    results: Result[] = [];
+    selectedResult: Result = new Result();
     constructor(private omdbService: OmdbService) { }
     getResults() {
-        this.omdbService.search().then(results => this.results = results);
+        this.omdbService.search(this.searchField).subscribe(results => this.results = results);
     }
     ngOnInit() {
         this.getResults();
--- app/omdb.service.ts
+++ app/omdb.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Inject } from '@angular/core';
 import { Result } from './result';
-import { RESULTS } from './mock-results';
-@Injectable()
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 export class OmdbService {
-    search(): Promise<Result[]> {
-        return Promise.resolve(RESULTS);
+    private url: string = 'http://www.omdbapi.com/?s=';
+
+    constructor(@Inject(Http) public http: Http) {
+
+    }
+
+    search(title): Observable<Result[]> {
+        return this.http.get(this.url + title)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.Search || [];
+    }
+
+    private handleError (error: any) {
+        // In a real world app, we might use a remote logging infrastructure
+        // We'd also dig deeper into the error to get a better message
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
     }
 }
