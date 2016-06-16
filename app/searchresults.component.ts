60031daf1dcb5c6e6e86d670e7a046279963372c
Allan Mørk Christensen <allanmc@gmail.com>
2016-06-16 12:54:49 +0200

    Added accordion for results

--- app/searchresults.component.ts
+++ app/searchresults.component.ts
@@ -1,16 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Result } from './result';
 import { SearchResultComponent } from './searchresult.component';
+import { Accordion } from 'primeng/primeng';
+import { AccordionTab } from 'primeng/primeng';
 
 @Component({
     selector: 'search-results',
     templateUrl: 'app/views/searchresults.html',
-    directives: [SearchResultComponent],
+    directives: [SearchResultComponent, Accordion, AccordionTab],
 })
 export class SearchResultsComponent {
     @Input()
     results: Result[];
-    selectedResult: Result;
-
-    onSelect(result: Result) { this.selectedResult = result; }
 } 
